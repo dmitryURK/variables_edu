4dad37bb7108f2c04a573d6ae989627b06a3234e
дмитрий <dmitryaltair@ukr.net>
2018-06-09 14:17:08 +0300

    Add variables

--- src/com/dmitryUKR/Main.java
+++ src/com/dmitryUKR/Main.java
@@ -1,8 +1,20 @@
 package com.dmitryUKR;
 
+/*
+
+    одна из первых моих программ изучаю испольщование переменных
+    и за одно учусь пользоваться GIT
+
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        byte bVar = 127;
+        short sVar = 1000;
+        long lVar = 2000;
+        float fVar = 200.10f;
+        double dVar = 400.10d;
+
+
     }
 }
