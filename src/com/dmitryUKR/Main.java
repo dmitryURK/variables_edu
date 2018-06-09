da1e1b64e5b935ed601799bd77e5b626af0dd701
дмитрий <dmitryaltair@ukr.net>
2018-06-09 14:25:31 +0300

    add comments

--- src/com/dmitryUKR/Main.java
+++ src/com/dmitryUKR/Main.java
@@ -9,11 +9,12 @@ package com.dmitryUKR;
 public class Main {
 
     public static void main(String[] args) {
-        byte bVar = 127;
-        short sVar = 1000;
-        long lVar = 2000;
-        float fVar = 200.10f;
-        double dVar = 400.10d;
+        byte bVar = 127;        // -127 до 127
+        short sVar = 1000;      // -32768 до 32767
+        int iVar = 1000;        // -2147483648 до 2147483647
+        long lVar = 2000;       // –9 223 372 036 854 775 808 до 9 223 372 036 854 775 807
+        float fVar = 200.10f;   // -3.4*1038 до 3.4*1038
+        double dVar = 400.10d;  // ±4.9*10-324 до ±1.8*10308.
 
 
     }
